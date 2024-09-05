756caf255e4583f721c422b56cf0321492c5922d
一晌贪欢 <291117803@qq.com>
2024-09-05 11:07:20 +0800

    Add files via upload

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  trailingComma: "es5",
+  singleQuote: false,
+  printWidth: 80,
+  tabWidth: 2,
+  bracketSameLine: true,
+};
---
+++ vue.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+// const { defineConfig } = require('@vue/cli-service')
+module.exports = {
+  chainWebpack: (config) => {
+    const types = ["vue-modules", "vue", "normal-modules", "normal"];
+    types.forEach((type) =>
+      addStyleResource(config.module.rule("less").oneOf(type))
+    );
+  },
+  devServer: {
+    proxy: {
+      "/prod-api": {
+        changeOrigin: true,
+        target: "https://demo.wiris.com", // 需要请求的目标接口
+      },
+    },
+  },
+};
+
+function addStyleResource(rule) {
+  rule
+    .use("style-resource")
+    .loader("style-resources-loader")
+    .options({
+      patterns: [path.resolve(__dirname, "./src/styles/global.less")],
+    });
+}
